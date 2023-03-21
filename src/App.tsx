f1eb9135ccd7f7543e59f8817162aae4614b4e87
JangJinseong <gaden7@naver.com>
2023-03-21 21:54:20 +0900

    rr

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    axios({
+      method: "post",
+      url: "/Game/ChampionRotation",
+    })
+    .then(res => res.data)
+    .then(data => console.log(data))
+  }, []);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.22
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Test</h1>
     </div>
   );
 }
